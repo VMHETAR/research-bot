f0d0562ca37f0fa7330e43f3849c5211264b0e52
VMHETAR <mhetarvarad@gmail.com>
2026-03-22 21:32:14 +0530

    Updated code.

--- code/RAG.py
+++ code/RAG.py
@@ -1,1 +1,9 @@
-import 
+import torch
+import os
+import httpx
+import json
+from langchain.chat_models import init_chat_model
+
+import bs4
+from langchain_community.document_loaders import WebBaseLoader
+
